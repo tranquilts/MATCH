cd50ef5c44c04500e3a92e51fbf637e7401fcb70
hafsahfreeaccount <hafsahhussain603@gmail.com>
2025-11-01 14:39:23 +0000

    settings update

---
+++ settings_menu.js
@@ -0,0 +1,19 @@
+function goToMainMenu(){
+    window.location.href = "menu.html"; // goes to main menu page
+}
+
+function resetGame(){
+    window.location.href = "gameplay.html"; // restarts the game
+}
+
+function quitGame(){
+    window.location.href = "goodbye.html"; // goes to goodbye page
+}
+
+document.querySelector('#btn-back').addEventListener('click', () => {
+    history.back();
+});
+
+document.getElementById('restart-btn').addEventListener('click', resetGame);
+document.getElementById('main-menu-btn').addEventListener('click', goToMainMenu);
+document.getElementById('quit-btn').addEventListener('click', quitGame);
