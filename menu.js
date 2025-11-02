40ab5b7651d27e3f5df19146b8baf87d35a9a855
khadijahi <109248890+tranquilts@users.noreply.github.com>
2025-11-02 23:29:29 +0000

    Fix errors in code

--- menu.js
+++ menu.js
@@ -1,29 +1,33 @@
-//Get reference to gameplay and setting menu screens
-const mainMenu = document.getElementById("menu-screen");
+//  Access the screens
+const mainMenu = document.getElementById("instructions-screen");
 const settingsMenu = document.getElementById("settings-screen");
 
-//Use buttons to navigate the screens
-const playBtn = document.getElementById("play-btn");
-const settingsBtn = document.getElementById("settings-icon");
+// Reference the buttons
+const playBtn = document.getElementById("start-game-btn");
+const settingsBtn = document.getElementById("settingsButton");
 const backBtn = document.getElementById("settings-back");
 
-//Function to change screens
-function switchScreen(show) {
+// Function to switch between screens
+function switchScreen(showScreen) {
+  // Hide both
   mainMenu.classList.add("hidden");
-  settingsScreen.classList.add("hidden");
-  show.classList.remove("hidden");
+  settingsMenu.classList.add("hidden");
 
-  //Play Now button
-  playBtn.addEventListener('click', ()=>{
-    window.location.href="gameplay.html";
-  });
-
-  //Open settings menu
-  settingsBtn.addEventListener('click', ()=> {
-    switchScreen(settingsScreen);
-  });
-
-  //Return to main menu from settings menu
-  backBtn.addEventListener('click', ()=>{
-    switchScreen(mainMenu); });
+  // Show the right screen
+  showScreen.classList.remove("hidden");
 }
+
+//  Add event listeners
+playBtn.addEventListener("click", () => {
+  // Go to gameplay page
+  window.location.href = "gameplay.html";
+});
+
+settingsBtn.addEventListener("click", () => {
+  switchScreen(settingsMenu);
+});
+
+backBtn.addEventListener("click", () => {
+  switchScreen(mainMenu);
+});
+
