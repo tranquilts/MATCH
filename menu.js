b0e433c540ac8f04501285fdb0854901e32adaa2
khadijahi <109248890+tranquilts@users.noreply.github.com>
2025-11-03 08:55:57 +0000

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
-// Play button goes to gameplay page
-document.getElementById("play-btn").addEventListener("click", () => {
-    window.location.href = "gameplay.html";
+// Go to gameplay page
+document.getElementById("playBtn").addEventListener("click", () => {
+  window.location.href = "gameplay.html";
 });
 
-// Settings icon goes to settings page
-document.getElementById("settings-button").addEventListener("click", () => {
-    window.location.href = "settings_menu.html";
+// Go to settings page
+document.getElementById("settings-btn").addEventListener("click", () => {
+  window.location.href = "settings.html";
 });
