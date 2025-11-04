b6ab3febd718a73c20800ef29509f55e97c4e1a0
khadijahi <109248890+tranquilts@users.noreply.github.com>
2025-11-04 00:46:55 +0000

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-// 🎮 Go to gameplay page
-document.getElementById("playBtn").addEventListener("click", () => {
-  window.location.href = "gameplay.html";
-});
-
-// ⚙️ Go to settings menu
-document.getElementById("settings-btn").addEventListener("click", () => {
-  window.location.href = "settings_menu.html";
-});
